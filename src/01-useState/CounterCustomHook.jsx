6f77b0d101537b4c28fe5e0b9ce8a4c0e4995132
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-13 22:01:28 -0500

    functionuseEffect start

--- src/01-useState/CounterCustomHook.jsx
+++ src/01-useState/CounterCustomHook.jsx
@@ -9,7 +9,7 @@ export const CounterCustomHook = () => {
             <h1>Counter With Custom Hook: {counter}</h1>
             <hr />
 
-            <button className="btn btn-primary" onClick={increment} >+1</button>
+            <button className="btn btn-primary" onClick={() => increment(2)} >+1</button>
             <button className="btn btn-primary" onClick={reset}>Reset</button>
             <button className="btn btn-primary" onClick={decrement}>-1</button>
         </>
--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -4,7 +4,7 @@ export const useCounter = (initialValue = 10) => {
 
     const [counter, setCounter] = useState(initialValue);
 
-    const increment = () => setCounter(counter + 1);
+    const increment = (value) => setCounter(counter + value);
     const decrement = () => setCounter(counter - 1);
     const reset = () => setCounter(initialValue);
 
